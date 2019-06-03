ac9f7fff4f7a7adbbe550928e8c3ba32ab10adda
Genie <gspaul6@gmail.com>
2019-06-03 11:40:13 +0200

    final commit

--- src/main/java/demo/jdbc/App.java
+++ src/main/java/demo/jdbc/App.java
@@ -15,23 +15,25 @@ public class App {
 		Class.forName("com.mysql.jdbc.Driver");
 		
 		// Etape 2 - Définition de la JDBC URL
-		String jdbcUrl = "jdbc:mysql://localhost:3306/formation-jpa";
+		String jdbcUrl = "jdbc:mysql://localhost:3306/bdd_mysql";
 		
 		// Etape 3 - Création de la connexion
-		Connection uneConnexion = DriverManager.getConnection(jdbcUrl, "root", "root");
+		Connection uneConnexion = DriverManager.getConnection(jdbcUrl, "root","");
 		
 		Statement st = uneConnexion.createStatement();
 		
 		// Etape 4 - exécution  de la requête
-		ResultSet rs = st.executeQuery("select * from LIVRE");
+		ResultSet rs = st.executeQuery("select * from clients");
 		
 		// Etape 5 - exploitation des résultats
 		while(rs.next()) {
 			
-			int id = rs.getInt("ID");
-			String titre = rs.getString("TITRE");
+			String code = rs.getString("CODE_POSTAL");
+			String ville = rs.getString("VILLE");
+			String societe=rs.getString("SOCIETE");
+		
 			
-			System.out.println("ID=" + id + " TITRE=" + titre);
+			System.out.println("CODE_POSTAL->" + code +"|"+ " SOCIETE = " + societe +"|"+ "   " + " VILLE = "+ ville );
 		}
 
 		// Etape 6 - Fermeture des ressources
